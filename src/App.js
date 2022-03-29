7088ebf98bda0d6b14f1a1463148919262cef10f
Jefferson Ding <jefferson.ding@ucc.on.ca>
2022-03-29 00:13:09 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useLocation } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NotFound from './pages/404';
