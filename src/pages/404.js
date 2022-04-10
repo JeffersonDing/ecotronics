241718c4f263dc9c22d8a734fbaf3ca6d4773bed
Jefferson Ding <jefferson.ding@ucc.on.ca>
2022-04-09 23:02:44 -0400

    homepage frontend

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
+    <div id="notfound">
       <h1>404</h1>
-      <p>Page not found</p>
+      <p>PǻġЁ nΩt ⌠ouñd</p>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import logo from '../img/logo.svg';
+import { Link } from 'react-router-dom';
 
 function Home() {
+  const generateGibberish = () => {
+    String.prototype.shuffle = function () {
+      var a = this.split(''),
+        n = a.length;
+
+      for (var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+      }
+      return a.join('');
+    };
+    const chars =
+      '          ±²³´µ¶·ËÌÍÎÏÐÑÒÓÔÕÖ×⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞←↑→↓↔↕↨∂∅∆∈∏∑−∕∙√∞∟∩∫≈≠≡≤≥⊙⌀⌂⌐⌠⌡─│┌┐└┘├┤┬ƒơƷǺǻǼ¸¹ļĽľĿŀŁłŃńŅņŇňŉŊŋŎŏŐÿĀāĂăĄąőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŰűΠΡΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώϐϴЀЁЂЃЄЅІЇЈЉЊЋЌЍЎЏŲųŴŵŶŷРСТУФХЦЧШЩЭЮЯабвгдŌōежзийклмнопрстуфхцчшщъыьэюяѐёђѓєѕº»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊǽǾØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýˉ˘˙˚˛˜˝;ŧŨũŪūŬŭŮů΄΅Ά·ΈΉΊΌΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟþĆćĈĉĊċČčĎЛМНОПďĐđĒēĔĕЪЫЬěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻіїјљњћќѝўџҐґ־אבײ׳״ᴛᴦᴨẀẁẂẃẄẅẟỲỳ‐‒–—―‗‘☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼!#$%&()*+,-./0123456789;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀ɑϐᴦᴨ∑ơµᴛɸϴΩẟ∞∅∈∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■’ŸŹźŻżŽžſ₧₪€℅ℓ№™Ω℮⅐⅑⅓⅔ǿȘגדהוזחטיךכלםמןנסעףפץצקרשתװױșȚțɑɸˆˇАБВГДЕЖЗИЙК‚‛“”„‟†‡•…‧‰′″‵‹›‼‾‿⁀⁄⁔⁴⁵⁶⁷⁸⁹⁺⁻ⁿ₁₂₃₄₅₆₇₈₉₊₋₣₤┴┼═║╒╓╔╕╖╗╘╙╚╛╜╝╞╟╠╡╢╣╤╥╦╧╨╩╪╫╬▀▁▄█▌▐░▒▓■□▪▫▬▲►▼◄◊○●◘◙◦☺☻☼♀♂♠♣♥♦♪♫✓ﬁﬂ�';
+    var gibberish = '';
+    for (let i = 0; i < 500; i++) {
+      //choose random char from chars
+      const rand = chars[Math.floor(Math.random() * chars.length)];
+      const spaces = ' '.repeat(Math.floor(Math.random() * 10));
+      gibberish += spaces + rand;
+    }
+    return gibberish.repeat(10);
+  };
   return (
-    <div className="Home">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/Home.js</code> and save to reload.
-      </p>
+    <div id="home">
+      <div className="gibberish">
+        <p>{generateGibberish()}</p>
+      </div>
+      <span className="title">
+        <h1>Ecotronics 2022</h1>
+        <h2>UCC BH Electronics Drive </h2>
+      </span>
+      <div id="entry-button">
+        <Link to="new">Enter Now...</Link>
+        <p className="blinker"></p>
+      </div>
     </div>
   );
 }
